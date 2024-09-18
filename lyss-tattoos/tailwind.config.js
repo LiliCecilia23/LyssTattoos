93cc49df6d6e81d51664e1ec49a430ab20674649
Lili Clift <liliclift@gmail.com>
2024-09-18 16:54:11 -0700

    booking form

--- lyss-tattoos/tailwind.config.js
+++ lyss-tattoos/tailwind.config.js
@@ -1,11 +1,12 @@
-const {nextui} = require('@nextui-org/theme');
+const {nextui} = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/components/(accordion|divider).js"
+    "./node_modules/@nextui-org/theme/dist/**/*.{js, ts, jsx, tsx}"
   ],
   theme: {
     extend: {
@@ -16,5 +17,6 @@ module.exports = {
       },
     },
   },
+  darkMode: "class",
   plugins: [nextui()],
 };
